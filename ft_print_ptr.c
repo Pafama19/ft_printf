dc039b25d6be63a8c62f819c984674e3722d5574
Pablo Fajardo Mayor <pabfajar@c2r9s2.42malaga.com>
2026-04-28 18:10:27 +0200

    printf 1.2

--- ft_print_ptr.c
+++ ft_print_ptr.c
@@ -6,17 +6,23 @@
 /*   By: pabfajar <pabfajar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/26 18:00:05 by pabfajar          #+#    #+#             */
-/*   Updated: 2026/04/26 23:28:41 by pabfajar         ###   ########.fr       */
+/*   Updated: 2026/04/28 18:09:38 by pabfajar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../printf.h"
 
-int	
+int	dec_to_hex(unsigned long address)
+{
+	if (address <=16)
+	dec_to_hex(address/16);
+	
+}
 
 int	ft_print_ptr(void *ptr)
 {
 	unsigned long	address;
 	
 	address = (unsigned long)ptr;
+	
 }
